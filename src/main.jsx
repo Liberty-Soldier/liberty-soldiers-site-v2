e2e66a77462d491775a4d8be1a8b073c084d2a7c
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 13:47:39 -0400

    Fix JSX nesting error in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,25 +8,25 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <nav
-  style={{
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: "10px",
-    padding: "10px",
-    backgroundColor: "#111",
-    color: "white",
-  }}
->
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/news" element={<ComingSoon title="News & Alerts" />} />
-        <Route path="/devotional" element={<ComingSoon title="Devotional" />} />
-        <Route path="/persecution-map" element={<ComingSoon title="Persecution Map" />} />
-        <Route path="/media" element={<ComingSoon title="Videos & Podcasts" />} />
-        <Route path="/community" element={<ComingSoon title="Community Forum" />} />
-      </Routes>
+      <div>
+        <nav style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", gap: "10px", padding: "10px", backgroundColor: "#111", color: "white" }}>
+          <Link to="/" style={{ color: "white" }}>Home</Link>
+          <Link to="/news" style={{ color: "white" }}>News</Link>
+          <Link to="/devotional" style={{ color: "white" }}>Devotional</Link>
+          <Link to="/persecution-map" style={{ color: "white" }}>Persecution Map</Link>
+          <Link to="/media" style={{ color: "white" }}>Media</Link>
+          <Link to="/community" style={{ color: "white" }}>Community</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/news" element={<ComingSoon title="News & Alerts" />} />
+          <Route path="/devotional" element={<ComingSoon title="Devotional" />} />
+          <Route path="/persecution-map" element={<ComingSoon title="Persecution Map" />} />
+          <Route path="/media" element={<ComingSoon title="Videos & Podcasts" />} />
+          <Route path="/community" element={<ComingSoon title="Community Forum" />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
+
