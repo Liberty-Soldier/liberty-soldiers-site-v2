a19d9723873e2b54a67de64a6f59d4cb2a6bc0bd
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 14:50:42 -0400

    Fix hero image layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,52 +1,45 @@
 import React from "react";
-import heroImage from "../assets/hero.jpg";
+import heroImage from "../assets/hero.jpg"; // Make sure the path is correct
 
 const HomePage = () => {
   return (
     <div
       style={{
-        backgroundImage: `url(${heroImage})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "top center",
-        backgroundSize: "contain",
-        backgroundColor: "black",
+        background: "linear-gradient(to right, #000000, #222222, #000000)",
         height: "100vh",
         width: "100%",
         display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        textAlign: "center",
-        paddingTop: "40px",
-        boxSizing: "border-box",
+        justifyContent: "center",
+        alignItems: "flex-start",
       }}
     >
-      <h1
+      <div
         style={{
-          fontSize: "3.5rem",
-          fontFamily: "'stencil', 'Arial Black', sans-serif",
-          textShadow: "2px 2px 0 black",
-          color: "tan",
-          marginBottom: "10px",
+          backgroundImage: `url(${heroImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "top center",
+          backgroundSize: "contain",
+          backgroundColor: "black",
+          height: "100vh",
+          width: "100%",
+          maxWidth: "1200px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "40px",
         }}
       >
-        LIBERTY SOLDIERS
-      </h1>
-      <p
-        style={{
-          fontSize: "1rem",
-	  fontFamily: "'stencil', 'Arial Black', sans-serif",
-          color: "tan",
-          backgroundColor: "rgba(0, 0, 0, 0.6)",
-          padding: "10px 20px",
-          borderRadius: "10px",
-        }}
-      >
-        Seeking truth, defending Torah & fighting deception
-      </p>
+        <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
+          Liberty Soldiers
+        </h1>
+        <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
+          Seeking truth, defending Torah, fighting deception.
+        </p>
+      </div>
     </div>
   );
 };
 
 export default HomePage;
+
 
