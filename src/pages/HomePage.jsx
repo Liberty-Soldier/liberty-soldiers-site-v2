839decc7d7d901a468c3b75e58b114abb5124847
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 15:23:05 -0400

    Switch to img element for hero image to fix zoom issues

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,24 +6,43 @@ const HomePage = () => {
   return (
     <div
   style={{
-    backgroundImage: `url(${heroImage})`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center top",
-    backgroundSize: "100% auto", // ✅ Full width, natural height
     backgroundColor: "black",
     height: "100vh",
     width: "100%",
-    maxWidth: "100%",
     overflow: "hidden",
     display: "flex",
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "center",
     textAlign: "center",
-    paddingTop: "40px",
     boxSizing: "border-box",
   }}
 >
+  <img
+    src={heroImage}
+    alt="Hero"
+    style={{
+      width: "100%",
+      height: "auto",
+      objectFit: "cover",
+      objectPosition: "top",
+    }}
+  />
+  <div
+    style={{
+      position: "absolute",
+      top: "40px",
+      width: "100%",
+      textAlign: "center",
+    }}
+  >
+    <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>Liberty Soldiers</h1>
+    <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
+      Seeking truth, defending Torah, fighting deception.
+    </p>
+  </div>
+</div>
+
       <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
         Liberty Soldiers
       </h1>
