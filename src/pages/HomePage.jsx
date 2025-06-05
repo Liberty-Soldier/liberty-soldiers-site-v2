570681003c22bcc68a7705e0f2d7be8a9576917d
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 14:27:50 -0400

    Fix hero image path for deployment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,44 +1,36 @@
-
 import React from "react";
-import heroImage from "..C:\Users\mklrs\Desktop\LibertySoldiers_Final_Fixed\public; 
+import heroImage from "../assets/hero.jpg";
 
 const HomePage = () => {
   return (
-    <div style={{ position: "relative", height: "100vh", width: "100vw", overflow: "hidden" }}>
-      <img
-        src={heroImage}
-        alt="Hero"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          zIndex: -1,
-        }}
-      />
-      <div
-        style={{
-          position: "relative",
-          zIndex: 1,
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          textAlign: "center",
-          paddingTop: "40px",
-          color: "white",
-        }}
-      >
-        <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>Liberty Soldiers</h1>
-        <p style={{ fontSize: "1.5rem", marginTop: "1rem" }}>
-          Seeking truth, defending Torah, fighting deception.
-        </p>
-      </div>
+    <div
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center top",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        backgroundColor: "black",
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        textAlign: "center",
+        paddingTop: "40px",
+        boxSizing: "border-box",
+      }}
+    >
+      <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
+        Liberty Soldiers
+      </h1>
+      <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
+        Seeking truth, defending Torah, fighting deception.
+      </p>
     </div>
   );
 };
 
 export default HomePage;
+
