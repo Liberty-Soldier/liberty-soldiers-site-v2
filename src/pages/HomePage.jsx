dc8646f7bfce4d259fed830a2a4162ff0c378403
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 14:10:04 -0400

    Adjust hero image sizing for full screen view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,52 +1,43 @@
 import React from "react";
-import heroImage from "/public/hero.jpg";
+import heroImage from "../assets/hero.jpg"; // or wherever it's located
 
 const HomePage = () => {
   return (
-<div
-  style={{
-    backgroundImage: `url(${heroImage})`,
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center",
-    backgroundSize: "contain",
-    backgroundColor: "black",
-    height: "100vh",
-    width: "100vw",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    textAlign: "center",
-    paddingTop: "40px",
-    boxSizing: "border-box",
-    backgroundAttachment: "fixed"	
-  }}
->    
-      <h1
+    <div style={{ position: "relative", height: "100vh", width: "100vw", overflow: "hidden" }}>
+      <img
+        src={heroImage}
+        alt="Hero"
         style={{
-          fontSize: "4rem",
-          fontFamily: "'stencil', 'Arial Black', sans-serif",
-          textShadow: "2px 2px 0 black",
-          color: "tan",
-          marginBottom: "10px",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          zIndex: -1,
+        }}
+      />
+      <div
+        style={{
+          position: "relative",
+          zIndex: 1,
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          textAlign: "center",
+          paddingTop: "40px",
+          color: "white",
         }}
       >
-        LIBERTY SOLDIERS
-      </h1>
-      <p
-        style={{
-          fontSize: "1rem",
-	  fontFamily: "'stencil', 'Arial Black', sans-serif",
-          color: "tan",
-          backgroundColor: "rgba(0, 0, 0, 0.6)",
-          padding: "10px 20px",
-          borderRadius: "10px",
-        }}
-      >
-        Seeking truth, defending Torah & fighting deception
-      </p>
+        <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>Liberty Soldiers</h1>
+        <p style={{ fontSize: "1.5rem", marginTop: "1rem" }}>
+          Seeking truth, defending Torah, fighting deception.
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default HomePage;
