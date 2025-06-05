bae1c5d2a4ee5c00be3327aa56ebb8adc37441f8
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 15:16:40 -0400

    Fix image path to use public folder

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,56 +1,39 @@
 import React from "react";
 
+const heroImage = "/hero.jpg"; // ✅ Public path
+
 const HomePage = () => {
   return (
     <div
       style={{
-        position: "relative",
-        width: "100%",
-        height: "100vh",
+        backgroundImage: `url(${heroImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center top",
+        backgroundSize: "cover",
         backgroundColor: "black",
-        overflow: "hidden",
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        textAlign: "center",
+        paddingTop: "40px",
+        boxSizing: "border-box",
       }}
     >
-      <img
-        src="/hero.jpg"
-        alt="Hero"
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "100%",
-          height: "100%",
-          objectFit: "contain",
-          zIndex: -1,
-        }}
-      />
-      <div
-        style={{
-          position: "relative",
-          zIndex: 1,
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          paddingTop: "40px",
-          textAlign: "center",
-        }}
-      >
-        <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
-          Liberty Soldiers
-        </h1>
-        <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
-          Seeking truth, defending Torah, fighting deception.
-        </p>
-      </div>
+      <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
+        Liberty Soldiers
+      </h1>
+      <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
+        Seeking truth, defending Torah, fighting deception.
+      </p>
     </div>
   );
 };
 
 export default HomePage;
+
 
 
 
