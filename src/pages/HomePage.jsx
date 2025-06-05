7aa86b78f3b8638d51728f9c26c03abd8452438b
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 13:11:15 -0400

    Fix hero image zoom issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,22 +4,24 @@ import heroImage from "/public/hero.jpg";
 const HomePage = () => {
   return (
     <div
-      style={{
-        backgroundImage: `url(${heroImage})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center top",
-        backgroundSize: "cover",
-        backgroundColor: "black",
-        height: "100vh",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        textAlign: "center",
-        paddingTop: "40px",
-        boxSizing: "border-box",
-      }}
+  style={{
+    backgroundImage: `url(${heroImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "contain", // Key: this prevents zooming
+    backgroundColor: "black",
+    height: "100vh",
+    width: "100vw", // Full screen width
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    textAlign: "center",
+    paddingTop: "40px",
+    boxSizing: "border-box",
+  }}
+>
     >
       <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>Liberty Soldiers</h1>
       <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
