ff2e4657868dd26f216a89d7a23013b79b0a7469
Liberty-Soldier <mklrss77@gmail.com>
2025-06-05 14:42:46 -0400

    Fix hero image layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,11 +21,28 @@ const HomePage = () => {
         boxSizing: "border-box",
       }}
     >
-      <h1 style={{ fontSize: "3rem", color: "tan", textShadow: "2px 2px black" }}>
-        Liberty Soldiers
+      <h1
+        style={{
+          fontSize: "3.5rem",
+          fontFamily: "'stencil', 'Arial Black', sans-serif",
+          textShadow: "2px 2px 0 black",
+          color: "tan",
+          marginBottom: "10px",
+        }}
+      >
+        LIBERTY SOLDIERS
       </h1>
-      <p style={{ fontSize: "1.5rem", color: "white", marginTop: "1rem" }}>
-        Seeking truth, defending Torah, fighting deception.
+      <p
+        style={{
+          fontSize: "1rem",
+	  fontFamily: "'stencil', 'Arial Black', sans-serif",
+          color: "tan",
+          backgroundColor: "rgba(0, 0, 0, 0.6)",
+          padding: "10px 20px",
+          borderRadius: "10px",
+        }}
+      >
+        Seeking truth, defending Torah & fighting deception
       </p>
     </div>
   );
