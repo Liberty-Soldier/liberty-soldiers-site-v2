350227ab035864aba8653b4203a1ff572487b730
Liberty-Soldier <mklrss77@gmail.com>
2025-10-18 08:18:57 -0400

    Update news.config.ts

--- lib/news.config.ts
+++ lib/news.config.ts
@@ -1,12 +1,17 @@
 // lib/news.config.ts
 export const NEWS_FEEDS: string[] = [
   "https://www.jpost.com/rss/rss.xml",
+  "https://endtimeheadlines.org/feed/",
+  "https://www.biometricupdate.com/feed",
+  "https://expose-news.com/feed/",
+  "https://www.nowtheendbegins.com/feed/",
+  "https://reclaimthenet.org/feed/",
+  "https://olivetreeviews.org/feed/",
   "https://www.allisrael.com/rss",
   "https://www.israeltoday.co.il/feed/",
   "https://www.prophecynewswatch.com/rss.xml",
   "https://www.theguardian.com/world/rss",
   "https://www.infowars.com/feed/",
-  "https://rss.infowars.com/TNT.xml",
   "https://www.israel365news.com/feed/",
   "https://www.timesofisrael.com/feed/",
 ];
@@ -18,3 +23,4 @@ export const PINNED_LINKS: { title: string; url: string; source?: string }[] = [
 export const BLACKLIST: string[] = [
   // "example.com"
 ];
+
