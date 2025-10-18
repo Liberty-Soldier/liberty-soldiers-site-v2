991f4ce57ed6226e064592ade9b3f36d4ce1e949
Liberty-Soldier <mklrss77@gmail.com>
2025-10-18 07:44:20 -0400

    Update ClearSW.tsx

--- components/ClearSW.tsx
+++ components/ClearSW.tsx
@@ -3,14 +3,22 @@ import { useEffect } from 'react';
 
 export default function ClearSW() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.getRegistrations?.().then(regs =>
-        regs.forEach(r => r.unregister())
-      );
-      if (window.caches?.keys) {
-        caches.keys().then(keys => keys.forEach(k => caches.delete(k)));
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      // Unregister all service workers
+      navigator.serviceWorker
+        .getRegistrations?.()
+        .then((regs) => regs.forEach((r) => r.unregister()))
+        .catch(() => {});
+
+      // Clear caches (defensive check to keep TS happy)
+      if (typeof window !== 'undefined' && 'caches' in window) {
+        caches
+          .keys()
+          .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+          .catch(() => {});
       }
     }
   }, []);
+
   return null;
 }
