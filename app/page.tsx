e82433c23da6fcdb8d03bbd7fe112d608c36e80f
Liberty-Soldier <mklrss77@gmail.com>
2026-01-13 10:05:22 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           <div className="aspect-video w-full rounded-2xl overflow-hidden border border-white/10">
             <iframe
               className="w-full h-full"
-              src="https://www.youtube.com/watch?v=WeFeWyonzgc"
+              src="https://youtu.be/WeFeWyonzgc"
               title="Latest Liberty Soldiers Video"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
@@ -50,4 +50,5 @@ export default function Home() {
   );
 
 }
+
 
