0174b38e2f1a33d88d3f27a4eed842b67a4b2098
Liberty-Soldier <mklrss77@gmail.com>
2026-02-09 13:19:13 -0500

    Update EmailBand.tsx

--- app/components/EmailBand.tsx
+++ app/components/EmailBand.tsx
@@ -2,7 +2,7 @@ import EmailSignup from "./EmailSignup";
 
 export default function EmailBand() {
   return (
-    <section className="border-b border-zinc-200 bg-zinc-50">
+    <section className="bg-zinc-50">
       <div className="mx-auto max-w-6xl px-6 py-3">
         <div className="flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
           <div className="min-w-0">
