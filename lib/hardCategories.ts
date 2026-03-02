d050a48d9bdcfae13d24a8acd0e75fd1659c088b
Liberty-Soldier <mklrss77@gmail.com>
2026-03-02 08:46:51 -0500

    Update hardCategories.ts

--- lib/hardCategories.ts
+++ lib/hardCategories.ts
@@ -22,6 +22,7 @@ export function toHardCategory(feedCategory?: string): HardCategory {
 
     case "middle-east":
     case "world":
+    case "iran-war":
       return "War & Geopolitics";
 
     case "religion":
